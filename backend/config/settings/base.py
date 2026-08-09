2d1934965ae3f97916d1577d0d6426583e6c7b5f
usami_renko <hihu@club.com>
2026-08-09 14:01:16 +0900

    add:.envの追加と環境変数の移動
    mysql_passとdjango_secret_keyを.envに移動しました。

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 from datetime import timedelta
 
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-yg*!zwy=i-h0o5x932^p*rp*(v)-))h23xfla*g)_dhh+-cq2d"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-yg*!zwy=i-h0o5x932^p*rp*(v)-))h23xfla*g)_dhh+-cq2d")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
