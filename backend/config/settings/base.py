29d6d83f0451af84a4fa7edb12d4e442bbe1e912
usami_renko <hihu@club.com>
2026-08-20 08:27:26 +0900

    fix:base.pyの環境変数のget()を修正

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -26,7 +26,7 @@ logger = logging.getLogger(__name__)
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -91,7 +91,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": os.environ.get("MYSQL_DATABASE", "app_db"),
         "USER": os.environ.get("MYSQL_USER", "root"),
-        "PASSWORD": os.environ.get["MYSQL_PASSWORD"],
+        "PASSWORD": os.environ.get("MYSQL_PASSWORD"),
         "HOST": "app-db",
         "PORT": "3306",
         "OPTIONS": {
